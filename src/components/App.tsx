d7ab12e9575f32281e9dac99ef529052ffe15942
Zahid Saif <zahidsaif1@outlook.com>
2024-05-31 12:35:28 +0530

    Made change for image to be imported

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,12 @@
 import ImageDrawingCanvas from "./ImageDrawingCanvas.tsx";
+import roofImage from "../assets/roof.webp"
 import "../styles/App.css"
 
 function App() {
     return (
         <>
             <h1 className="main-heading">Canvas Assignment</h1>
-            <ImageDrawingCanvas imgSrc="/src/assets/roof.webp" />
+            <ImageDrawingCanvas imgSrc={roofImage} />
         </>
     )
 }
